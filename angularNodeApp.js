9d0d1be6a2996ba9a24c7832e34a944e93fd699d
adil7a <adil.akh.kz@gmail.com>
2019-12-30 19:48:41 +0100

    Adil's part done

--- angularNodeApp.js
+++ angularNodeApp.js
@@ -25,11 +25,39 @@ db.connect((err) => {
   if (err) throw err;
   
   console.log('Connected!');
-}); 
+});
+
+app.get('/addParcel', function(req, res) {
+  let queryCustomer = 'SELECT custId, custName, custLocation FROM Customers';
+  let queryLocation = 'SELECT locId, locAddress, city FROM Locations';
+  db.query(queryCustomer, function(err, resultsCus){
+    if (err) throw err;
+    db.query(queryLocation, function(err, resultsLoc){
+      if (err) throw err;
+      res.setHeader('Content-Type', 'text/html');
+      res.render('ngParcel', {customers: resultsCus, locations: resultsLoc });
+    })
+  })
+});
+
+app.get('/insertParcel', function(req, res) {
+  let weight = (req.query.txt_weight);
+  let cus    = (req.query.txt_cus);
+  let loc    = (req.query.txt_loc);
+  let sql = 'INSERT INTO Parcels (weight, custId, finalLocation) VALUES(?, ?, ?)';
+  let values = [weight, cus, loc];
+  // create web content once insert statement is processed
+  let postProcessInsert =   function (err, result) {
+    if (err) throw err;
+    res.setHeader('Content-Type', 'text/html');
+    res.render('parcelsConfirm', {weight: weight, cus: cus, loc:loc, insertedLines: result.affectedRows, id: result.insertId});
+  };
+  db.query(sql, values, postProcessInsert);
+});
 
 // load routes: define cont roller which act on db
 let routes = require('./components/routes.js');
 routes(app, db);
 
-// run server  
-app.listen(3003);
+// run server
+app.listen(3001);
